c50e833b7152c8348d14e8ce54ef60741e07d334
Kashish <kashyapkashish22@gmail.com>
2024-03-20 10:41:29 +0530

    frontend

--- frontend/src/components/Login.js
+++ frontend/src/components/Login.js
@@ -1,43 +1,50 @@
-import React, {useState} from 'react'
-import {useHistory} from 'react-router-dom'
+import React, { useState } from 'react'
+import { useHistory } from 'react-router-dom'
 
 const Login = (props) => {
-    let history= useHistory();
-    const [credentials, setCredentials] = useState({email:"", password:""})
-    const handleSubmit=async(e)=>{
-        e.preventDefault();
-        const response = await fetch(`http://localhost:5000/v1/api/auth/login`, {
-            method: "POST",
-            headers: {
-              "Content-Type": "application/json",
-            },
-            body: JSON.stringify({email: credentials.email, password: credentials.password})
-          });
-          const json= await response.json();
-          console.log(json);
-          if(json.success){
-            localStorage.setItem('token', json.authToken)
-            history.push('/');
-            props.showAlert("Account created Successfully", "success")
-          }else{
-            props.showAlert("Invalid Credentials", "danger")
-          }
+  let history = useHistory();
+  const [credentials, setCredentials] = useState({ email: "", password: "" })
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const response = await fetch(`http://localhost:5000/v1/api/auth/login`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ email: credentials.email, password: credentials.password })
+    });
+    const json = await response.json();
+    console.log(json);
+    if (json.success) {
+      localStorage.setItem('token', json.authToken)
+      history.push('/');
+      props.showAlert("Account created Successfully", "success")
+    } else {
+      props.showAlert("Invalid Credentials", "danger")
     }
-    const onChange=(e)=>{
-        setCredentials({...credentials, [e.target.name]: e.target.value})
-      }
+  }
+  const onChange = (e) => {
+    setCredentials({ ...credentials, [e.target.name]: e.target.value })
+  }
   return (
-    <form onSubmit={handleSubmit}>
+    <div className="container mt-5">
+      <div className="text-center">
+        <h1 className="text-info">Welcome Back</h1>
+        <p className="lead form-text mb-4">Log in to your account to continue</p>
+      </div>
+      <form onSubmit={handleSubmit} style={{ maxWidth: "400px", margin: "auto" }}>
         <div className="mb-3">
-            <label htmlFor="email" className="form-label">Email address</label>
-            <input type="email" className="form-control" id="email" onChange={onChange} value={credentials.email} name="email" aria-describedby="emailHelp"/>
+          <label htmlFor="email" className="form-label">Email address</label>
+          <input type="email" className="form-control" id="email" onChange={onChange} value={credentials.email} name="email" aria-describedby="emailHelp" />
         </div>
         <div className="mb-3">
-            <label htmlFor="password" className="form-label">Password</label>
-            <input type="password" className="form-control" onChange={onChange} value={credentials.password} id="password" name="password"/>
+          <label htmlFor="password" className="form-label">Password</label>
+          <input type="password" className="form-control" onChange={onChange} value={credentials.password} id="password" name="password" />
         </div>
-        <button type="submit" className="btn btn-primary" >Submit</button>
-</form>
+        <button type="submit" className="btn btn-primary">Submit</button>
+      </form>
+    </div>
+
   )
 }
 
